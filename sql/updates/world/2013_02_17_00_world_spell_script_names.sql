bfe793b9f1d5e3b7008525b3917e917cbee8bd76
RisingCore <dan_hngn@yahoo.com>
2013-04-23 09:03:25 +0300

    update spell_script_names

--- sql/updates/world/2013_02_17_00_world_spell_script_names.sql
+++ sql/updates/world/2013_02_17_00_world_spell_script_names.sql
@@ -1,6 +1,8 @@
+DELETE FROM `spell_script_names` WHERE `spell_id`=78;
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_sindragosa_frost_breath';
 DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_item_unsated_craving';
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
+(78,'spell_warr_heroic_strike'),
 (69649,'spell_sindragosa_frost_breath'),
 (71056,'spell_sindragosa_frost_breath'),
 (71057,'spell_sindragosa_frost_breath'),
